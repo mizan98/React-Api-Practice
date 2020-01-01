488411346a059809902937dd2c0111048ff17593
Mizan Choudhury <mizan_98@hotmail.co.uk>
2020-01-01 20:06:09 +0000

    api added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,36 @@ import './App.css';
 
 class App extends Component {
 
+  constructor (props) {
+    super (props)
+    this.state = {
+      items = [],
+      isloaded = false,
+    }
+  }
+
+    //---- Used to call the api ----//
+    componentDidMount () {
+
+      //---- Fetch the data ----//
+      fetch ('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+
+        //---- Put API information via setSate ----//
+        .then(json => {
+          this.setState({
+            items = json,
+            isloaded = true
+          })
+        })
+
+    }
+  
   render () {
     return (
       <div className='App'>
 
-        
+
       </div>
     )
   }
